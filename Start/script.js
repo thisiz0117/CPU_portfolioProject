fb5be8911a4c8fea7d50408e5842f7bcd76ed3dd
Thisiz0117 <musicicon065@gmail.com>
2025-09-10 08:34:34 +0900

    시작페이지 호버애니메이션 지랄났는데 포기함

---
+++ Start/script.js
@@ -0,0 +1,38 @@
+//   &-through {
+//     transition: 0.6s cubic-bezier(0.77, 0, 0.175, 1); // ease-in-out-quartic;
+
+//     &::before {
+//       position: absolute;
+//       content: "";
+//       top: 0;
+//       left: 0;
+//       z-index: -1;
+//       width: 100%;
+//       height: 100%;
+//       background: var(--btn-bg);
+//       transform: scaleX(0);
+//       transform-origin: right;
+//       transition: transform 0.6s cubic-bezier(0.77, 0, 0.175, 1);
+//     }
+
+//     &:hover {
+//       color: white;
+//       background: transparent;
+
+//       &::before {
+//         transform: scaleX(1);
+//         transform-origin: left;
+//       }
+//     }
+//   }
+
+$(document).ready(function() {
+    // 처음엔 위쪽에 배치
+    $('.contant-container').css('top', '60%');
+    $('.contant-container').css('opacity', '0');
+    // 0.2초 후 가운데로 이동
+    $('.contant-container').delay(200).animate({
+        opacity: 1,
+        top: '50%'
+    }, 600, 'easeOutExpo');
+});
